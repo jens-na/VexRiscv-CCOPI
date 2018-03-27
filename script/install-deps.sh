0b3362bd806453bd008ba451553ba4cb226e2d08
Jens Nazarenus <me@jens-na.de>
2018-03-27 16:48:23 +0200

    Add RISC-V toolchain dependency to Travis

--- script/install-deps.sh
+++ script/install-deps.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# RISC-V toolchain dependencies
+sudo apt-get install autoconf automake autotools-dev curl device-tree-compiler libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev
+
+# RISC-V toolchain
+git clone https://github.com/riscv/riscv-tools.git
+cd riscv-tools
+git submodule update --init --recursive
+export RISCV=/opt/riscv
+./build-rv32ima.sh
+cd ..
+
 # Verilator
-sudo apt-get install git make autoconf g++ flex bison
-git clone http://git.veripool.org/git/verilator   # Only first time
-unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
-unset VERILATOR_ROOT  # For bash
+git clone http://git.veripool.org/git/verilator
+unset VERILATOR_ROOT
 cd verilator
-git pull        # Make sure we're up-to-date
-git tag v3.872  # See what versions exist
-autoconf        # Create ./configure script
+git pull       
+git checkout "v3.872"
+autoconf        
 ./configure
 make
 sudo make install
+cd ..
 
 # SpinalHDL
-git clone https://github.com/SpinalHDL/SpinalHDL.git
-cd SpinalHDL
-sbt clean compile publish-local
+#git clone https://github.com/SpinalHDL/SpinalHDL.git
+#cd SpinalHDL
+#sbt clean compile publish-local
