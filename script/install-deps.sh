5960848c69d7cb3ca5adf1639a11281191cbb2ea
Jens Nazarenus <me@jens-na.de>
2018-03-28 21:03:15 +0200

    Try selfcompiled version of libisl15

--- script/install-deps.sh
+++ script/install-deps.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# RISC-V toolchain
-sudo apt-get install autoconf automake autotools-dev curl device-tree-compiler libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libisl
+# libisl15
+wget http://isl.gforge.inria.fr/isl-0.15.tar.gz
+tar -xvf isl-0.15.tar.gz
+cd isl-0.15
+./configure
+make
+sudo make install
+
+# RISC-V toolchain (precompiled version)
+sudo apt-get install autoconf automake autotools-dev curl device-tree-compiler libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev
 curl -O -L "https://github.com/jens-na/riscv-tools-precompiled/blob/master/builds/build-rv32ima.tar.gz.[00-14]?raw=true"
 sudo mkdir -p /opt/riscv
 sudo cat build-rv32ima.tar.gz.* | sudo tar xzf - -C /opt/riscv
-
-find /opt/riscv
 
 # Verilator
 git clone http://git.veripool.org/git/verilator
